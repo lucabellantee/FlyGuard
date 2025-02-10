cc2259a52ba27372d04f7a51be27f252c7656454
lucabellantee <s1097955@studenti.univpm.it>
2025-02-10 15:43:27 +0100

    Pulizia codice pt.2

--- model_inference.py
+++ model_inference.py
@@ -49,16 +49,14 @@ class ModelInference:
         """
         Calcola e stampa le metriche della regressione.
         """
-        #mae = mean_absolute_error(y_true, y_pred)
-        #mse = mean_squared_error(y_true, y_pred)
-        #rmse = mean_squared_error(y_true, y_pred, squared=False)
-        #r2 = r2_score(y_true, y_pred)
+        mae = mean_absolute_error(y_true, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
+        r2 = r2_score(y_true, y_pred)
 
-        #print("\n📊 **Metriche Regressione**")
-        #print(f"MAE: {mae:.4f}")
-        #print(f"MSE: {mse:.4f}")
-        #print(f"RMSE: {rmse:.4f}")
-        #print(f"R² Score: {r2:.4f}")
+        print("\n📊 **Metriche Regressione**")
+        print(f"MAE: {mae:.4f}")
+        print(f"MSE: {mse:.4f}")
+        print(f"R² Score: {r2:.4f}")
 
     def evaluate_classification(self, y_true, y_pred):
         """
